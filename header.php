294766de5bbb74d83fb163b19bf0ca35f6b9a8af
zachisit <info@zachis.it>
2015-12-09 17:20:56 -0500

    responsive and general code cleanup
    finished a lot of the responsive work related to images; added content
    for about; added header for contact; general cleanup of source

--- header.php
+++ header.php
@@ -42,11 +42,11 @@ $(window).load(function(){
 	</div><!--header_top-->
 	<div id="header_bottom">
 		<div id="header_bottom_right">
-		
-	<label for="show-menu" class="show-menu">Show Menu</label>
+		<label for="show-menu" class="show-menu">Show Menu</label>
 		<input type="checkbox" id="show-menu" role="button">
 		<ul id="menu">
-		<li><a href="about.php" title="">About</a></li><li><a href="websites.php" title="">Websites</a></li><li><a href="mobile.php" title="">Mobile</a></li><li><a href="lifestream.php" title="">Lifestream</a></li><li><a href="contact.php" title="">Contact</a></li></ul>
+			<li><a href="about.php" title="">About</a></li><li><a href="websites.php" title="">Websites</a></li><li><a href="mobile.php" title="">Mobile</a></li><li><a href="lifestream.php" title="">Lifestream</a></li><li><a href="contact.php" title="">Contact</a></li>
+		</ul>
 		</div><!--#header_bottom_right-->
 	</div><!--header-top-->
 	</div><!--header-->
